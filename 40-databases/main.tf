b7447c4046229f4c24ed3a8ed4aea239c9e12496
narendrakumar-sun <narendra.sunkara83@gmail.com>
2026-04-19 20:14:39 +0400

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -118,3 +118,44 @@ resource "terraform_data" "mysql" {
      ]
   }
 }
+
+
+resource "aws_instance" "rabbitmq" {
+  ami           = local.ami_id
+  instance_type = "t3.micro"
+  subnet_id = local.database_subnet_id
+  vpc_security_group_ids = [local.rabbitmq_sg_id]
+#   iam_instance_profile = aws_iam_instance_profile.mongod.name
+
+  tags = merge(
+    {
+        Name = "${var.project}-${var.environment}-rabbitmq"
+    },
+    local.common_tags
+  )
+}
+
+
+resource "terraform_data" "rabbitmq" {
+  triggers_replace = [
+    aws_instance.rabbitmq.id
+  ]
+   connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.rabbitmq.private_ip
+  }
+    provisioner "file" {
+    source      = "boostrap.sh"    # Path on local machine
+    destination = "/tmp/boostrap.sh"   # Path on remote machine
+  }
+
+
+  provisioner "remote-exec" {
+    inline = [ 
+        "chmod +x /tmp/boostrap.sh",
+        "sudo sh /tmp/boostrap.sh rabbitmq"
+     ]
+  }
+}
