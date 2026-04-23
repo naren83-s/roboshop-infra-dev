762cfc17b6558484744647742de160b91363d988
narendrakumar-sun <narendra.sunkara83@gmail.com>
2026-04-23 14:36:04 +0400

    terraform infra

---
+++ 50-backend-alb/variables.tf
@@ -0,0 +1,14 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+  default = "Z0354649BHBBW98BVSKE"
+}
+variable "domain_name" {
+  default = "naren83.online"
+}
---
+++ 60-catalogue/main.tf
@@ -0,0 +1,39 @@
+resource "aws_instance" "catalogue" {
+  ami           = local.ami_id
+  instance_type = "t3.micro"
+  subnet_id = local.private_subnet_id
+  vpc_security_group_ids = [local.catalogue_sg_id]
+#   iam_instance_profile = aws_iam_instance_profile.mongod.name
+
+  tags = merge(
+    {
+        Name = "${var.project}-${var.environment}-catalogue"
+    },
+    local.common_tags
+  )
+}
+
+
+resource "terraform_data" "catalogue" {
+  triggers_replace = [
+    aws_instance.catalogue.id
+  ]
+   connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.catalogue.private_ip
+  }
+    provisioner "file" {
+    source      = "boostrap.sh"    # Path on local machine
+    destination = "/tmp/boostrap.sh"   # Path on remote machine
+  }
+
+
+  provisioner "remote-exec" {
+    inline = [ 
+        "chmod +x /tmp/boostrap.sh",
+        "sudo sh /tmp/boostrap.sh catalogue dev"
+     ]
+  }
+}
